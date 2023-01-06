394070ba7cabbf23ba5364a5acb725fd6ec90e45
sandip <sandipbhandari805@gmail.com>
2023-01-06 22:27:59 +0530

    Javascript session

--- 17.08.2022/Q.4.js
+++ 17.08.2022/Q.4.js
@@ -1,4 +1,16 @@
 //Problem 4 –
 //Write a program to find the HCF or GCD of two integers
 
-(function () {})();
+(function () {
+  function getHCF(firstNumber, secondNumber) {
+    for (let i = 1; i <= firstNumber && i <= secondNumber; i++) {
+      if (firstNumber % i == 0 && secondNumber % i == 0) {
+        HCF = i;
+      }
+    }
+    return HCF;
+  }
+
+  console.log(getHCF(16, 8));
+  console.log(getHCF(15, 18));
+})();
