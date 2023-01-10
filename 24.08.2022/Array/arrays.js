a41ec2e294bd91f00e57f34b11dd42c7784297ad
sandip <sandipbhandari805@gmail.com>
2023-01-10 14:25:06 +0530

    Simple array

---
+++ 24.08.2022/Array/arrays.js
@@ -0,0 +1,10 @@
+(function () {
+  let sandip = [];
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  for (let number of numbers) {
+    if (number % 2 == 0) {
+      console.log(number);
+      console.log(sandip);
+    }
+  }
+})();
