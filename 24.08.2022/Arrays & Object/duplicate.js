61e87511d9d37afd56906f9bea4af11d8ff285e1
sandip <sandipbhandari805@gmail.com>
2023-01-10 17:27:17 +0530

    Javascript session

--- 24.08.2022/Arrays & Object/duplicate.js
+++ 24.08.2022/Arrays & Object/duplicate.js
@@ -1,3 +1,7 @@
+// Remove the duplicate element in the array
+// input: const arr = ['a','b','c','a','c','d','a'];
+// output: ['a','b','c','d']
+
 (function () {
   function unique(arr) {
     let uniqObj = {};
