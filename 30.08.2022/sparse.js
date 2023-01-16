3f12e7dc99e6cba1926d314494a96d56828ef419
sandip <sandipbhandari805@gmail.com>
2023-01-16 12:31:52 +0530

    Index file

---
+++ 30.08.2022/sparse.js
@@ -0,0 +1,30 @@
+//TODO: SPARSE ARRAY
+
+// Given a 2D array. Our task is to check if the array is sparse or not.
+// Sparse Matrix - A matrix is said to be sparse if the number of zeros is more than half of the total elements.
+
+(function () {
+  function checkIfSparse(multiArr) {
+    let counter = 0;
+    let rowCount = multiArr.length;
+    let columnCount = 0;
+    for (let row = 0; row < multiArr.length; row++) {
+      columnCount = multiArr[row].length;
+      for (let col = 0; col < multiArr[row].length; col++) {
+        if (multiArr[row][col] == 0) {
+          counter++;
+        }
+      }
+    }
+
+    return counter > parseInt((rowCount * columnCount) / 2);
+  }
+
+  let multidimensional = [
+    [1, 0, 0, 0],
+    [6, 0, 0, 67],
+    [1, 0, 45, 0],
+    [1, 0, 0, 0],
+  ];
+  console.log("Is Array Sparse " + checkIfSparse(multidimensional));
+})();
