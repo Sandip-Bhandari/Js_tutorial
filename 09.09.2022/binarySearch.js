79c8499f9c9509a555cb38e04836ffa934076254
sandip <sandipbhandari805@gmail.com>
2023-01-20 18:46:13 +0530

    12 sept

---
+++ 09.09.2022/binarySearch.js
@@ -0,0 +1,18 @@
+function binarySearch(arr, item, start, end) {
+  middle = parseInt((end + start) / 2);
+
+  if (arr[middle] == item) {
+    return middle;
+  }
+
+  if (arr[middle] < item) {
+    start = middle + 1;
+  } else {
+    end = middle - 1;
+  }
+
+  return binarySearch(arr, item, start, end);
+}
+
+let numbers = [10, 20, 20, 20, 50, 60, 70, 80, 90, 100];
+console.log(binarySearch(numbers, 20, 0, numbers.length - 1));
