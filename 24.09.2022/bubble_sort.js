7376fd571b543adafd9c856fefeab2e1cabe5bf3
sandip <sandipbhandari805@gmail.com>
2023-01-23 11:52:52 +0530

    problems on sort

--- 24.09.2022/bubble_sort.js
+++ 24.09.2022/bubble_sort.js
@@ -1,29 +1,29 @@
-function bubbleSort(arr){
+function bubbleSort(arr) {
   let N = arr.length;
   let swapped = false;
-  for(let i = 0; i < N - 1; i++) {
-      swapped = false;
-      for(let j =0; j < N; j++) {
-          if(arr[j] > arr[j + 1]) {
-              swap(arr, j, j+1);
-              swapped = true;
-          }
+  for (let i = 0; i < N - 1; i++) {
+    swapped = false;
+    for (let j = 0; j < N; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        swapped = true;
       }
+    }
 
-      if(!swapped) {
-          break;
-      }
+    if (!swapped) {
+      break;
+    }
   }
 
   console.log("Count " + count);
   return arr;
 }
 
-function swap(arr,a,b) {
+function swap(arr, a, b) {
   let temp = arr[a];
   arr[a] = arr[b];
   arr[b] = temp;
 }
 
-let arr = [1,0,3,6,7];
-console.log(bubbleSort(arr));~
+let arr = [1, 0, 3, 6, 7];
+console.log(bubbleSort(arr));
