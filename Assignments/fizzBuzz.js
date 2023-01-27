2456cc15045a02fc81375e1d82586582cd3c7739
sandip <sandipbhandari805@gmail.com>
2023-01-27 14:52:35 +0530

    competetive code

--- Assignments/fizzBuzz.js
+++ Assignments/fizzBuzz.js
@@ -1,3 +1,10 @@
+// To print "FizzBuzz" if i is a multiple of both 3 and 5,
+// "Fizz" if i is a multiple of 3 (but not 5),
+// "Buzz" if i is a multiple of 5 (but not 3), and the
+// value of i if it is not a multiple of 3 or 5 in
+// javascript, you can use a for loop and the modulus
+// operator to check for divisibility. The syntax would be as follows:
+
 (function () {
   let n = 15;
   for (let i = 1; i <= n; i++) {
