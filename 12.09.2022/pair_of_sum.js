0b1d224d55524a060fafc8de7265ecdcd5e86914
sandip <sandipbhandari805@gmail.com>
2023-01-29 21:16:08 +0530

    12 sept

--- 12.09.2022/pair_of_sum.js
+++ 12.09.2022/pair_of_sum.js
@@ -1,24 +1,29 @@
-function getPairOfSum(arr, sum) {
-  let dictionary = {};
-  for (let i = 0; i < arr.length; i++) {
-    if (dictionary[arr[i]]) {
-      return {
-        a: arr[i],
-        b: dictionary[arr[i]],
-      };
-    } else {
-      dictionary[sum - arr[i]] = arr[i];
+// Find the pair of elements in a given array A such
+// that the sum of the pair is equal to N.
+
+(function () {
+  function getPairOfSum(arr, sum) {
+    let dictionary = {};
+    for (let i = 0; i < arr.length; i++) {
+      if (dictionary[arr[i]]) {
+        return {
+          a: arr[i],
+          b: dictionary[arr[i]],
+        };
+      } else {
+        dictionary[sum - arr[i]] = arr[i];
+      }
     }
+
+    return "Pair not found";
   }
 
-  return "Pair not found";
-}
+  let arr = [10, 20, 40, 50, 60, 70];
+  console.log(getPairOfSum(arr, 50));
 
-let arr = [10, 20, 40, 50, 60, 70];
-console.log(getPairOfSum(arr, 50));
+  arr = [10, 20, 40, 50, 60, 70];
+  console.log(getPairOfSum(arr, 70));
 
-arr = [10, 20, 40, 50, 60, 70];
-console.log(getPairOfSum(arr, 70));
-
-arr = [10, 20, 40, 50, 60, 70];
-console.log(getPairOfSum(arr, 190));
+  arr = [10, 20, 40, 50, 60, 70];
+  console.log(getPairOfSum(arr, 190));
+})();
