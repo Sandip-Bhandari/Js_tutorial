12d425d6677423b4b84303d26b03fe84693d018c
sandip <sandipbhandari805@gmail.com>
2023-01-29 21:18:44 +0530

    12 sept

--- 12.09.2022/unique.js
+++ 12.09.2022/unique.js
@@ -1,25 +1,29 @@
-function getUniqueElement(arr) {
-  let dictionary = {};
-  for (let i = 0; i < arr.length; i++) {
-    if (dictionary[arr[i]] == undefined) {
-      dictionary[arr[i]] = 1;
-    } else {
-      dictionary[arr[i]] += 1;
+// Given an array of lenght N, where every element is present twice
+// and only one element is present once. Find the unique element.
+(function () {
+  function getUniqueElement(arr) {
+    let dictionary = {};
+    for (let i = 0; i < arr.length; i++) {
+      if (dictionary[arr[i]] == undefined) {
+        dictionary[arr[i]] = 1;
+      } else {
+        dictionary[arr[i]] += 1;
+      }
     }
+
+    console.log(dictionary);
   }
 
-  console.log(dictionary);
-}
+  function getUniqueByBitWise(arr) {
+    let output = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      output = output ^ arr[i];
+    }
 
-function getUniqueByBitWise(arr) {
-  let output = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    output = output ^ arr[i];
+    return output;
   }
 
-  return output;
-}
-
-let arr = [21, 13, 21, 90, 90, 12, 12];
-getUniqueElement(arr);
-console.log(getUniqueByBitWise(arr));
+  let arr = [21, 13, 21, 90, 90, 12, 12];
+  getUniqueElement(arr);
+  console.log(getUniqueByBitWise(arr));
+})();
